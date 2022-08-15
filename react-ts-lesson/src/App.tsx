fe5905065d6b5938115dbe6c314282f8aa0792a4
SinkLine_P <popovartem505@gmail.com>
2022-08-15 18:10:34 +0300

    create Interface IUser and show on page

--- react-ts-lesson/src/App.tsx
+++ react-ts-lesson/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Card, {CardVariant} from "./components/Card";
+import UserList from "./components/UserList";
 
 const App = () => {
+  const users: IUser[] = [
+    {id: 1, name: "Artem", email: "pfdsfpsdp@gmail.com", address: {city: "Moscow", street: "Lenina", zipcode: "12345"}},
+    {id: 2, name: "Misha", email: "pfcvcxvxcvcxvdp@gmail.com", address: {city: "SPB", street: "Kirova", zipcode: "09876"}},
+  ]
   return (
     <div>
-      This is App File
+      <Card onClick={(num) => console.log("click ", num)} variant={CardVariant.outlined} width={"200px"} height={"100px"}>
+        <button>Create</button>
+      </Card>
+      <UserList users={users} />
     </div>
   );
 };
